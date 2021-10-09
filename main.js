138cd7a4bad53d414b4032a7770e88660aba2ac7
Thiago Nascimento <70329966+Thiagosn1@users.noreply.github.com>
2021-10-08 21:27:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const nav = document.querySelector('#header nav');
-const icons = document.querySelector('nav .toggle');
 
-for(const element of icons) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show');
-    })
+const nav = document.querySelector('#header nav')
+const icons = document.querySelectorAll('nav .toggle')
+
+for(const element of icons){
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
 }
 
 const links = document.querySelectorAll('nav ul li a');
 
-for( const link of link) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show');
-    })
+for( const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show');
+  })
 }
